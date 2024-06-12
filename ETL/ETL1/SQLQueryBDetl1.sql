681c9e94803c8e155dd1dd8d02fb1ff8094380c4
Alejandro Bovet <102919972+AlejoBovet@users.noreply.github.com>
2024-06-11 21:36:03 -0400

    ETL1 V0.2.0
    Se genera query para crear base de datos junto con diagrama y se generan marckdowns para organizar proceso en cuaderno.

---
+++ ETL/ETL1/SQLQueryBDetl1.sql
@@ -0,0 +1,51 @@
+
+
+CREATE TABLE Casts (
+	Cast_id INT PRIMARY KEY ,
+	Name_cast Varchar(50)
+);
+
+CREATE TABLE Type_show (
+	Type_show_id INT PRIMARY KEY,
+	Type_show Varchar(50)NOT NULL
+);
+
+CREATE TABLE Caracteristics (
+	Caracteristic_id INT PRIMARY KEY,
+	Listen_id Varchar(50),
+	Description_caract Varchar(500)
+
+);
+
+CREATE TABLE Date_show_added (
+	Date_show_added_id INT PRIMARY KEY,
+	Fecha DATE,
+	Year_added INT,
+	Month_added INT,
+	Day_added INT
+);
+
+CREATE TABLE Date_show_release (
+	Date_show_release_id INT PRIMARY KEY,
+	Year_release INT
+);
+
+
+CREATE TABLE Shows (
+	Show_id VARCHAR(4) PRIMARY KEY,
+	Title VARCHAR(70) NOT NULL,
+	Country VARCHAR(50),
+	Rating VARCHAR(5),
+	Director VARCHAR(70),
+	Cast_id INT,
+	Type_show_id INT,
+	Caracteristic_id INT,
+	Date_show_added_id INT,
+	Date_show_release_id INT,
+	CONSTRAINT FK_Cast FOREIGN KEY (Cast_id) REFERENCES Casts(Cast_id),
+	CONSTRAINT FK_Type_show FOREIGN KEY (Type_show_id) REFERENCES Type_show(Type_show_id),
+	CONSTRAINT FK_Caracteristic FOREIGN KEY (Caracteristic_id) REFERENCES Caracteristics(Caracteristic_id),
+	CONSTRAINT FK_Show_added FOREIGN KEY (Date_show_added_id) REFERENCES Date_show_added(Date_show_added_id),
+	CONSTRAINT FK_Show_release FOREIGN KEY (Date_show_release_id) REFERENCES Date_show_release(Date_show_release_id)
+
+);
