b92424ae67ab86acf3786650252c4bcca6297d99
Alejandro Bovet <102919972+AlejoBovet@users.noreply.github.com>
2024-06-17 23:57:26 -0400

    ETL1 V0.3.1
    Corrección a Query, se agrega función de auto incrementable  a las tablas que están en punta respecto a la tabla principal.

--- ETL/ETL1/SQLQueryBDetl1.sql
+++ ETL/ETL1/SQLQueryBDetl1.sql
@@ -1,24 +1,24 @@
 
 
 CREATE TABLE Casts (
-	Cast_id INT PRIMARY KEY ,
+	Cast_id INT IDENTITY(1,1) PRIMARY KEY ,
 	Name_cast Varchar(50)
 );
 
 CREATE TABLE Type_show (
-	Type_show_id INT PRIMARY KEY,
+	Type_show_id INT IDENTITY(1,1) PRIMARY KEY,
 	Type_show Varchar(50)NOT NULL
 );
 
 CREATE TABLE Caracteristics (
-	Caracteristic_id INT PRIMARY KEY,
+	Caracteristic_id INT IDENTITY(1,1) PRIMARY KEY,
 	Listen_id Varchar(50),
 	Description_caract Varchar(500)
 
 );
 
 CREATE TABLE Date_show_added (
-	Date_show_added_id INT PRIMARY KEY,
+	Date_show_added_id INT IDENTITY(1,1) PRIMARY KEY,
 	Fecha DATE,
 	Year_added INT,
 	Month_added INT,
@@ -26,7 +26,7 @@ CREATE TABLE Date_show_added (
 );
 
 CREATE TABLE Date_show_release (
-	Date_show_release_id INT PRIMARY KEY,
+	Date_show_release_id INT IDENTITY(1,1) PRIMARY KEY,
 	Year_release INT
 );
 
